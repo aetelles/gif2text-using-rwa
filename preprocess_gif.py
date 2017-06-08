c554e79005d6ffeeb86e366fd4bf649e728d2390
aetelles <ajetchamathegreat@gmail.com>
2017-06-08 12:08:41 +0800

    Create preprocess_gif.py

--- preprocess_gif.py
+++ preprocess_gif.py
@@ -5,14 +5,14 @@ import os
 from PIL import Image
 # import scikitimage
 
-from inception_preprocessing.py import preprocess_for_eval
+from inception_preprocessing import preprocess_for_eval
 from cnn_inception_resnet import *
 
 slim = tf.contrib.slim
 
 save_path = './tgif_dataset/'
 train_path = './tgif_dataset/train/'
-test_path = './tgif_dataset/test'
+test_path = './tgif_dataset/test/'
 
 
 def create_gif_list(train_mode):
@@ -20,8 +20,6 @@ def create_gif_list(train_mode):
     # downloads the frame and then transforms it into an array
     print("Preprocessing frames for CNN...")
 
-    dataset_li = []
-    data_path = 'string'
     if train_mode == True:
         data_path = train_path
     else:
@@ -55,7 +53,7 @@ def preprocess_frame(frame_file):
     frame = 2*(frame/255.0)-1.0 # normalization???
     frame = preprocess_for_eval(frame, height=227, width=227) # consider changing this to preprocess_for_training
     # not really sure about line 57 because the preprocessing might be doubled
-    # anyway line 57 crops the frame i think lol
+    # anyway line 56 crops the frame i think lol
     return frame
 
 def create_dataframe():
@@ -82,3 +80,42 @@ def create_dataframe():
     test_set = pd.DataFrame({'gif_path': test_list, 'description': test_captions})
 
     return train_set, test_set
+
+def main():
+    num_frames = 150
+
+    cnn = CNN_inception(batch_size=20, 227, 227, 3)
+
+    train_list = create_gif_list(train_mode=True)
+    test_list = create_gif_list(train_mode=False)
+    for gif_path in train_list:
+        frame_list = create_frame_list(gif_path)
+        frame_count = len(frame_list)
+        frame_list = np.array(frame_list)
+
+        if frame_count > 150:
+            frame_indices = np.linspace(0, frame_count, num=num_frames, endpoint=False).astype(int)
+            frame_list = frame_list[frame_indices]
+
+        processed_frame_list = np.array(map(lambda x: preprocess_frame(x), frame_list))
+        features = cnn.extract_features(processed_frame_list)
+
+        gif_feats_path = os.path.join(train_path, gif_path)
+        gif_feats_path = os.path.join(gif_feats_path, 'feats.npy')
+        np.save(gif_feats_path, features)
+    for gif_path in test_list:
+        frame_list = create_frame_list(gif_path)
+
+        if len(frame_list) > 80:
+            frame_indices = np.linspace(0, len(frame_list), num=num_frames, endpoint=False).astype(int)
+            frame_list = frame_list[frame_indices]
+
+        processed_frame_list = np.array(map(lambda x: preprocess_frame(x), frame_list))
+        features = cnn.extract_features(processed_frame_list)
+
+        gif_feats_path = os.path.join(test_path, gif_path)
+        gif_feats_path = os.path.join(gif_feats_path, 'feats.npy')
+        np.save(gif_feats_path, features)
+
+if __name__="__main__":
+    main()
